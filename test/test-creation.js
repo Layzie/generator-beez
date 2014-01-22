db6de223fd6435a85e58e033a42b1ebe59e4422f
HIRAKI Satoru <saruko313@gmail.com>
2014-01-23 02:19:23 +0900

    fix indent

--- test/test-creation.js
+++ test/test-creation.js
@@ -1,3 +1,4 @@
+/* vim: set tabstop=2 shiftwidth=2 softtabstop=2 expandtab : */
 /*global describe, beforeEach, it*/
 'use strict';
 
@@ -6,55 +7,55 @@ var helpers = require('yeoman-generator').test;
 
 
 describe('beez generator', function () {
-    beforeEach(function (done) {
-        helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
-            if (err) {
-                return done(err);
-            }
+  beforeEach(function (done) {
+    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+      if (err) {
+        return done(err);
+      }
 
-            this.app = helpers.createGenerator('beez:app', [
-                '../../app'
-            ]);
-            this.app.options['skip-install'] = true;
+      this.app = helpers.createGenerator('beez:app', [
+        '../../app'
+      ]);
+      this.app.options['skip-install'] = true;
 
-            done();
-        }.bind(this));
+      done();
+    }.bind(this));
+  });
+
+  it('creates expected files', function (done) {
+    var expected = [
+      // add files you expect to exist here.
+      '.jshintrc',
+      '.jshintignore',
+      'Gruntfile.js',
+      'package.json',
+      'setver',
+      'LICENSE',
+      'README.md',
+      's/beez-mymodule/index.js',
+      's/beez-mymodule/model/index.js',
+      's/beez-mymodule/view/index.js',
+      'deps/beez',
+      'deps/beez/s/beez/index.js',
+      'spec/index.js',
+      'spec/all.js',
+      'spec/all.html',
+      'spec/foundation/spec.js',
+      'spec/require-config.js'
+    ];
+
+    helpers.mockPrompt(this.app, {
+      'name': 'beez-mymodule',
+      'description': 'awesome module',
+      'license': 'MIT',
+      'githubUsername': 'octocat',
+      'authorName': 'Octo Cat',
+      'authorEmail': 'octo@example.com'
     });
 
-    it('creates expected files', function (done) {
-        var expected = [
-            // add files you expect to exist here.
-            '.jshintrc',
-            '.jshintignore',
-            'Gruntfile.js',
-            'package.json',
-            'setver',
-            'LICENSE',
-            'README.md',
-            's/beez-mymodule/index.js',
-            's/beez-mymodule/model/index.js',
-            's/beez-mymodule/view/index.js',
-            'deps/beez',
-            'deps/beez/s/beez/index.js',
-            'spec/index.js',
-            'spec/all.js',
-            'spec/all.html',
-            'spec/foundation/spec.js',
-            'spec/require-config.js'
-        ];
-
-        helpers.mockPrompt(this.app, {
-            'name': 'beez-mymodule',
-            'description': 'awesome module',
-            'license': 'MIT',
-            'githubUsername': 'octocat',
-            'authorName': 'Octo Cat',
-            'authorEmail': 'octo@example.com'
-        });
-
-        this.app.run({}, function () {
-            helpers.assertFiles(expected);
-            done();
-        });
+    this.app.run({}, function () {
+      helpers.assertFiles(expected);
+      done();
     });
+  });
 });
--- test/test-load.js
+++ test/test-load.js
@@ -1,11 +1,12 @@
+/* vim: set tabstop=2 shiftwidth=2 softtabstop=2 expandtab : */
 /*global describe, beforeEach, it*/
 'use strict';
 
 var assert = require('assert');
 
 describe('beez generator', function () {
-    it('can be imported without blowing up', function () {
-        var app = require('../app');
-        assert(app !== undefined);
-    });
+  it('can be imported without blowing up', function () {
+    var app = require('../app');
+    assert(app !== undefined);
+  });
 });
